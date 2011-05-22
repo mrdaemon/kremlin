4ef5d9ae7a571f97242cf2cc44e539d039486549
Alexandre Gauthier <alex@underwares.org>
2011-05-22 19:04:33 -0400

    Add proper banner to dev launch script

--- runserver.py
+++ runserver.py
@@ -14,8 +14,11 @@
 from kremlin import app
 
 def main():
-    print "Launching kremlin in development mode."
-    print "--------------------------------------"
+    print "Kremlin Magical Everything System v 0.0.0-None"
+    print "Copyright (c) Glasnost 2010-2011"
+    print "-----------------------------------------------"
+    print "RUNNING IN DEVELOPMENT MODE! ** NOT FOR PRODUCTION **"
+    print "Connect to http://127.0.0.1:5000 to access."
     app.run(debug=True)
 
 if __name__ == '__main__':
