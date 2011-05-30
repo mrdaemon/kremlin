e921cc8d11e4937409dfd3e2943e9559b2b3f87c
Alexandre Gauthier <alex@lab.underwares.org>
2011-05-30 03:14:00 -0400

    Add preliminary work for a yaml based configuration file.
    I am temporarily shelving this because it is a lot of work, and I could
    probably get away with something much, much more simple that would work
    just as well, and is also built in to Flask, using the Configure object.
    
    I'll come back to this later if I feel it would be better.

--- runserver.py
+++ runserver.py
@@ -10,15 +10,38 @@
                Glasnost Image Board and Boredom Inhibitor
 
 """
+import sys
+
+from yaml import load
+
+# Attempt to use the libyaml C loader, fallback to pure python
+try:
+    from yaml import CLoader as Loader
+except ImportError:
+    from yaml import Loader
 
 from kremlin import app
+
+configdata = None
+
+def readconfigdata(profile="development"):
+    try:
+        configdata = load(file('configuration.yml').read(), Loader=Loader)
+    except IOError:
+        print "Unable to read 'configuration.yml'!"
+        print "Ensure it is present in the current directory and try again."
+        sys.exit(1)
+
+    if configdata.has_key(profile):
+        print "Loading configuration data for %s..." % (profile)
+        config = configdata[profile]
+
+        internal_values = ['DEBUG','TESTING','SECRET_KEY',]
 
 def main():
     print "Kremlin Magical Everything System v 0.0.0-None"
     print "Copyright (c) Glasnost 2010-2011"
     print "-----------------------------------------------"
-    print "RUNNING IN DEVELOPMENT MODE! ** NOT FOR PRODUCTION **"
-    print "Connect to http://127.0.0.1:5000 to access."
     app.run(debug=True)
 
 if __name__ == '__main__':
