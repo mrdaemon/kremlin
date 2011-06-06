e75f0cc300c8619cd458f9edcfeb181a5902491d
Alexandre Gauthier <alex@underwares.org>
2011-06-06 06:32:14 -0400

    Partial commit of add_image() logic, unfinished.

--- kremlin/core.py
+++ kremlin/core.py
@@ -9,10 +9,12 @@
                Glasnost Image Board and Boredom Inhibitor
 
 """
+import hashlib
 
-from flask import request, session, render_template, flash
+from flask import request, session, render_template, flash, url_for, redirect
+from werkzeug import secure_filename
 
-from kremlin import app, forms
+from kremlin import app, db, dbmodel, forms
 
 @app.route('/')
 def home_index():
@@ -25,14 +27,51 @@ def entries_index():
     return render_template('board.html', form=forms.NewPostForm())
 
 @app.route('/post/<int:post_id>')
-def post_id(post_id):
+def view_post(post_id):
     """ Show post identified by post_id """
-    return "Post view: %s" % (post_id)
+    post = dbmodel.Post.query.filter_by(id=post_id).get_or_404()
+    # TODO: Write template for post views.
+    return "Post view: %s" % (post)
 
-@app.route('/add', methods=['POST'])
+@app.route('/add/', methods=['POST'])
 def add_image():
     """ Add a new image """
-    return "Add a new image"
+
+    form = forms.NewPostForm()
+
+    if form.validate_on_submit():
+        filename = secure_filename(form.upload.filename)
+        filedata = form.upload.stream.read()
+
+        h = hashlib.new('sha1')
+        h.update(filedata)
+        filehash = h.hexdigest()
+
+        # Validate file uniqueness
+        dupe = dbmodel.Image.query.filter_by(sha1sum=filehash).first()
+
+        if dupe is not None:
+            flash("Image already exists: %r" % (dupe))
+            return redirect(url_for('entries_index'))
+        else:
+            # File is unique, proceed to create post and image.
+            dbimage = dbmodel.Image(filename, filehash)
+            db.session.add(dbimage)
+
+            user = ""
+
+            if "username" in session:
+                user = session['username']
+            else:
+                user = form.name
+
+            note = form.note
+            tags =
+
+
+
+
+
 
 @app.route('/login', methods=['GET','POST'])
 def login():
