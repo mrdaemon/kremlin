b6e86fcb40556926addd515841aa8f78dae564b7
Alexandre Gauthier <alex@lab.underwares.org>
2011-07-10 09:30:57 -0400

    Add scaffold for imgutils module

---
+++ kremlin/imgutils.py
@@ -0,0 +1,18 @@
+"""
+               #   # ####  ##### #   # ##### #   # #   #
+               #  #  #   # #     ## ##  #  # #  ## #   #
+               ###   ####  ####  # # #  #  # # # # #####
+               #  #  #     #     #   #  #  # ##  # #   #
+               #   # #     ##### #   # #   # #   # #   #
+
+                   Kremlin Magical Everything System
+               Glasnost Image Board and Boredom Inhibitor
+
+"""
+
+from PIL import Image
+
+def mkthumb(imgfile):
+    """docstring for mkthumb"""
+    #TODO: Actually write this HERP DERP
+    # I am adding it here just so I remember to do so.
